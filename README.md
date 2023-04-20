e8407649746471a2ed96b05656354eacebd80c8b
David Halls <dahalls@gmail.com>
2023-04-20 20:58:29 +0100

    Update for latest spec

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+![CI
+status](https://github.com/davedoesdev/matroska-schema/workflows/ci/badge.svg)
+
 Replacement for
 [`schema.js`](https://github.com/oeuillot/node-matroska/blob/master/lib/schema.js)
 from [node-matroska](https://github.com/oeuillot/node-matroska), which
@@ -10,6 +13,6 @@ and is MIT-licensed.
 
 This schema is in [schema.js](schema.js).
 
-To generate it on your own machine, run [gen\_schema.sh](gen_schema.sh).
+To generate it on your own machine, run [gen_schema.sh](gen_schema.sh).
 You’ll need Node.js and wget. The script will complain if the result
 differs from node-matroska’s schema.
--- diff_schema.js
+++ diff_schema.js
@@ -1,17 +1,56 @@
 const schema = require('./schema.js');
 const matroska_schema = require('matroska/lib/schema');
 
-matroska_schema.byEbmlID[matroska_schema.byName.Slices].maxver = 0;
-matroska_schema.byEbmlID[matroska_schema.byName.CRC_32].description += '.';
-matroska_schema.byEbmlID[matroska_schema.byName.EBMLMaxIDLength].range = '>=' + matroska_schema.byEbmlID[matroska_schema.byName.EBMLMaxIDLength].range;
-matroska_schema.byEbmlID[matroska_schema.byName.EBMLMaxSizeLength].range = 'not 0';
-matroska_schema.byEbmlID[matroska_schema.byName.SignedElement].level = 2;
-matroska_schema.byEbmlID[matroska_schema.byName.SignatureAlgo].level = 2;
-matroska_schema.byEbmlID[matroska_schema.byName.SignatureHash].level = 2;
-matroska_schema.byEbmlID[matroska_schema.byName.SignaturePublicKey].level = 2;
-matroska_schema.byEbmlID[matroska_schema.byName.Signature].level = 2;
-matroska_schema.byEbmlID[matroska_schema.byName.SignatureSlot].level = 1;
-matroska_schema.byEbmlID[matroska_schema.byName.EBML].multiple = false;
+matroska_schema.byEbmlID[matroska_schema.byName.ChapLanguageIETF].name = 'ChapLanguageBCP47';
+matroska_schema.byEbmlID[matroska_schema.byName.ChapLanguageIETF].cppname = 'ChapLanguageIETF';
+matroska_schema.byName.ChapLanguageBCP47 = matroska_schema.byName.ChapLanguageIETF;
+delete matroska_schema.byName.ChapLanguageIETF;
+
+matroska_schema.byEbmlID[matroska_schema.byName.TagLanguageIETF].name = 'TagLanguageBCP47';
+matroska_schema.byEbmlID[matroska_schema.byName.TagLanguageIETF].cppname = 'TagLanguageIETF';
+matroska_schema.byName.TagLanguageBCP47 = matroska_schema.byName.TagLanguageIETF;
+delete matroska_schema.byName.TagLanguageIETF;
+
+matroska_schema.byEbmlID[matroska_schema.byName.LanguageIETF].name = 'LanguageBCP47';
+matroska_schema.byEbmlID[matroska_schema.byName.LanguageIETF].cppname = 'LanguageIETF';
+matroska_schema.byName.LanguageBCP47 = matroska_schema.byName.LanguageIETF;
+delete matroska_schema.byName.LanguageIETF;
+
+matroska_schema.byEbmlID[matroska_schema.byName.FileMimeType].name = 'FileMediaType';
+matroska_schema.byName.FileMediaType = matroska_schema.byName.FileMimeType;
+delete matroska_schema.byName.FileMimeType;
+
+matroska_schema.byEbmlID[matroska_schema.byName.ChapterSegmentUID].name = 'ChapterSegmentUUID';
+matroska_schema.byEbmlID[matroska_schema.byName.ChapterSegmentUID].cppname = 'ChapterSegmentUID';
+matroska_schema.byName.ChapterSegmentUUID = matroska_schema.byName.ChapterSegmentUID;
+delete matroska_schema.byName.ChapterSegmentUID;
+
+matroska_schema.byEbmlID[matroska_schema.byName.SegmentUID].name = 'SegmentUUID';
+matroska_schema.byEbmlID[matroska_schema.byName.SegmentUID].cppname = 'SegmentUID';
+matroska_schema.byName.SegmentUUID = matroska_schema.byName.SegmentUID;
+delete matroska_schema.byName.SegmentUID;
+
+matroska_schema.byEbmlID[matroska_schema.byName.PrevUID].name = 'PrevUUID';
+matroska_schema.byEbmlID[matroska_schema.byName.PrevUID].cppname = 'PrevUID';
+matroska_schema.byName.PrevUUID = matroska_schema.byName.PrevUID;
+delete matroska_schema.byName.PrevUID;
+
+matroska_schema.byEbmlID[matroska_schema.byName.NextUID].name = 'NextUUID';
+matroska_schema.byEbmlID[matroska_schema.byName.NextUID].cppname = 'NextUID';
+matroska_schema.byName.NextUUID = matroska_schema.byName.NextUID;
+delete matroska_schema.byName.NextUID;
+
+matroska_schema.byEbmlID[matroska_schema.byName.OldStereoMode].maxver = 2;
+matroska_schema.byEbmlID[matroska_schema.byName.PrimaryRChromaticityX].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.PrimaryRChromaticityY].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.PrimaryGChromaticityX].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.PrimaryGChromaticityY].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.PrimaryBChromaticityX].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.PrimaryBChromaticityY].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.WhitePointChromaticityX].range = '0x0p+0-0x1p+0';
+matroska_schema.byEbmlID[matroska_schema.byName.WhitePointChromaticityY].range = '0x0p+0-0x1p+0';
+delete matroska_schema.byEbmlID[matroska_schema.byName.ChapterSegmentUUID].range;
+delete matroska_schema.byEbmlID[matroska_schema.byName.SegmentUUID].range;
 
 for (let p in matroska_schema.byEbmlID) {
     const v = matroska_schema.byEbmlID[p].default;
@@ -22,19 +61,22 @@ for (let p in matroska_schema.byEbmlID) {
 }
 
 for (let k in matroska_schema.byName) {
-    if (matroska_schema.byName[k] !== schema.byName[k]) {
-        throw new Error(`missing element: ${k}`);
+    const v1 = matroska_schema.byName[k];
+    const v2 = schema.byName[k];
+    if (v1 !== v2) {
+        throw new Error(`missing element: ${k} 0x${v1.toString(16)}|0x${v2.toString(16)}`);
     }
 }
 
 for (let k in matroska_schema.byEbmlID) {
     const v = matroska_schema.byEbmlID[k];
     for (p in v) {
-        const v1 = v[p];
-        const v2 = schema.byEbmlID[k][p];
-        if (v1 !== v2) {
-            throw new Error(`mismatch: ${v.name}.${p} ${v1}|${v2}`);
+        if (p !== 'description') {
+            const v1 = v[p];
+            const v2 = schema.byEbmlID[k][p];
+            if (v1 !== v2) {
+                throw new Error(`mismatch: ${v.name}.${p} |${v1}|${v2}|`);
+            }
         }
     }
 }
-
